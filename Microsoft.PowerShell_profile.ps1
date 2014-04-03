d8f83c2df9641761010363f4f043fd9946480a83
Jelle de Jong <jelle@hoest.nl>
2014-04-03 10:54:04 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -11,14 +11,37 @@ $env:DefaultServer = "IP-ONTW05"
 # some aliasses
 Set-Alias build "C:\development\intern\powershell\scripts\Invoke-Build.ps1"
 Set-Alias buildngo "C:\development\intern\powershell\scripts\Invoke-BuildDevelop.ps1"
-Set-Alias fsi "c:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\fsi.exe"
+Set-Alias fsi "c:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe"
+Set-Alias fsx "c:\Program Files (x86)\Microsoft SDKs\F#\3.1\Framework\v4.0\fsi.exe"
 Set-Alias goto "C:\development\intern\powershell\scripts\Open-Project.ps1"
-Set-Alias gvim "C:\Program Files (x86)\Vim\vim74\gvim.exe"
-Set-Alias iprox "C:\development\intern\powershell\scripts\Invoke-StartServer.ps1"
-Set-Alias subl "C:\Program Files\Sublime Text 2\sublime_text.exe"
+Set-Alias gt goto
+Set-Alias gvim_exe "C:\Program Files (x86)\Vim\vim74\gvim.exe"
+Set-Alias run "C:\development\intern\powershell\scripts\Invoke-StartServer.ps1"
+Set-Alias ll "Get-ChildItem"
+Set-Alias subl "C:\Program Files\Sublime Text 3\sublime_text.exe"
 Set-Alias sudo "C:\development\intern\powershell\scripts\Sudo.ps1"
-Set-Alias vim "C:\Program Files (x86)\Vim\vim74\gvim.exe"
+Set-Alias tc_exe "c:\totalcmd\TOTALCMD64.EXE"
+Set-Alias vim gvim
 Set-Alias which "Get-Command"
+
+Function tc {
+  if($args) {
+    $resolved = resolve-path $args
+    tc_exe "/o" $resolved
+  }
+  else {
+    tc_exe "/o"
+  }
+}
+
+Function gvim {
+  if($args) {
+    gvim_exe --remote-silent $args
+  }
+  else {
+    gvim_exe
+  }
+}
 
 # a simple prompt for powershell (with use of Posh-Git)
 Function prompt {
@@ -40,7 +63,7 @@ Function profile {
 # with this command, your editor ('gvim') will start with the hostsfile
 # you need to run it from an elevated powershell
 Function hostsfile {
-  sudo gvim "C:\Windows\System32\drivers\etc\hosts"
+  sudo "C:\Program Files (x86)\Vim\vim74\gvim.exe" "C:\Windows\System32\drivers\etc\hosts"
 }
 
 # gets the powershell version
